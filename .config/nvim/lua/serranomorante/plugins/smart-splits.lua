d4d76b6bd2018e5532048625fbc739b039f7b753
Patricio Serrano <serranomorantepatricio@gmail.com>
2023-08-24 23:51:03 +0200

    Fix vim not preserving splits aspect ratio on terminal resize

--- .config/nvim/lua/serranomorante/plugins/smart-splits.lua
+++ .config/nvim/lua/serranomorante/plugins/smart-splits.lua
@@ -1,38 +1,43 @@
 return {
-	"mrjones2014/smart-splits.nvim",
-	dependencies = {
+	{
 		"kwkarlwang/bufresize.nvim",
+		event = "VeryLazy",
+		config = true,
 	},
-	keys = {
-		{
-			"<leader>rs",
-			function()
-				require("smart-splits").start_resize_mode()
-			end,
+
+	{
+		"mrjones2014/smart-splits.nvim",
+		keys = {
+			{
+				"<leader>rs",
+				function()
+					require("smart-splits").start_resize_mode()
+				end,
+			},
 		},
-	},
-	opts = function()
-		return {
-			ignored_filetypes = {
-				"nofile",
-				"quickfix",
-				"prompt",
-				"neo-tree",
-				"harpoon",
-				"NvimTree",
-			},
-			resize_mode = {
-				silent = true,
-				hooks = {
-					on_leave = function()
-						require("bufresize").register()
-						vim.notify("Smart Splits: RESIZE MODE OFF", vim.log.levels.INFO)
-					end,
-					on_enter = function()
-						vim.notify("Smart Splits: RESIZE MODE ON", vim.log.levels.WARN)
-					end,
+		opts = function()
+			return {
+				ignored_filetypes = {
+					"nofile",
+					"quickfix",
+					"prompt",
+					"neo-tree",
+					"harpoon",
+					"NvimTree",
 				},
-			},
-		}
-	end,
+				resize_mode = {
+					silent = true,
+					hooks = {
+						on_leave = function()
+							require("bufresize").register()
+							vim.notify("Smart Splits: RESIZE MODE OFF", vim.log.levels.INFO)
+						end,
+						on_enter = function()
+							vim.notify("Smart Splits: RESIZE MODE ON", vim.log.levels.WARN)
+						end,
+					},
+				},
+			}
+		end,
+	},
 }
