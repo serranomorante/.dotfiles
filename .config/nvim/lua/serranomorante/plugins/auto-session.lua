3780bc78b734a80593c9724cddfa10c067b6cb54
Patricio Serrano <serranomorantepatricio@gmail.com>
2023-09-30 20:08:14 +0200

    Fix auto-session and telescope lazy loading

--- .config/nvim/lua/serranomorante/plugins/auto-session.lua
+++ .config/nvim/lua/serranomorante/plugins/auto-session.lua
@@ -1,3 +1,6 @@
+local augroup = vim.api.nvim_create_augroup
+local autocmd = vim.api.nvim_create_autocmd
+
 local function notify_restore()
 	vim.notify("Session restored", vim.log.levels.INFO)
 end
@@ -8,6 +11,8 @@ end
 
 return {
 	"rmagatti/auto-session",
+	dependencies = "nvim-telescope/telescope.nvim",
+	lazy = true,
 	opts = {
 		log_level = "error",
 		auto_session_root_dir = vim.fn.stdpath("data") .. "/sessions/",
@@ -20,18 +25,39 @@ return {
 		auto_session_use_git_branch = false,
 		session_lens = {
 			path_display = { "shorten" },
+			load_on_setup = false,
 		},
 		post_restore_cmds = { notify_restore },
 		post_save_cmds = { notify_save },
 	},
+	init = function()
+		autocmd("User LazyLoad", {
+			desc = "Lazy setup session lens when loading telescope.nvim",
+			group = augroup("AutoSession", { clear = true }),
+			callback = function(event)
+				if event and event.data == "telescope.nvim" then
+					require("auto-session").setup_session_lens()
+				end
+			end,
+		})
+	end,
 	keys = {
 		{
 			"<leader>xr",
 			"<cmd>SessionRestore<CR>",
+			desc = "Restore session",
 		},
 		{
 			"<leader>xs",
 			"<cmd>SessionSave<CR>",
+			desc = "Save session",
+		},
+		{
+			"<leader>xf",
+			function()
+				require("auto-session.session-lens").search_session()
+			end,
+			desc = "Search session",
 		},
 	},
 }
