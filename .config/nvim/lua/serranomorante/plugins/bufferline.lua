deac56514b6aa62768e688146508367024fe8b9b
Patricio Serrano <serranomorantepatricio@gmail.com>
2023-10-29 23:29:24 +0100

    Add `nvim-web-devicons` and enable buffer icons in bufferline

--- .config/nvim/lua/serranomorante/plugins/bufferline.lua
+++ .config/nvim/lua/serranomorante/plugins/bufferline.lua
@@ -23,7 +23,7 @@ return {
 			},
 			mode = "tabs",
 			enforce_regular_tabs = false,
-			show_buffer_icons = false,
+			show_buffer_icons = true,
 			show_buffer_close_icons = false,
 			show_close_icon = false,
 			indicator = {
