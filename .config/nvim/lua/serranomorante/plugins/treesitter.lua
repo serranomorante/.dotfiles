c8c64f30a2e40531da8d3f98f20b5380a372c376
Patricio Serrano <serranomorantepatricio@gmail.com>
2023-11-16 09:26:32 +0100

    Some treesitter improvements

--- .config/nvim/lua/serranomorante/plugins/treesitter.lua
+++ .config/nvim/lua/serranomorante/plugins/treesitter.lua
@@ -22,34 +22,39 @@ return {
 		"TSUpdateSync",
 	},
 	build = ":TSUpdate",
+	init = function(plugin)
+		-- PERF: add nvim-treesitter queries to the rtp and it's custom query predicates early
+		-- This is needed because a bunch of plugins no longer `require("nvim-treesitter")`, which
+		-- no longer trigger the **nvim-treeitter** module to be loaded in time.
+		-- Luckily, the only thins that those plugins need are the custom queries, which we make available
+		-- during startup.
+		-- CODE FROM LazyVim (thanks folke!) https://github.com/LazyVim/LazyVim/commit/1e1b68d633d4bd4faa912ba5f49ab6b8601dc0c9
+		require("lazy.core.loader").add_to_rtp(plugin)
+		require("nvim-treesitter.query_predicates")
+	end,
 	opts = {
 		ensure_installed = {
-			-- markdown
-			-- "markdown",
-			-- shell
+			-- HACK: force install of shipped neovim parsers since TSUpdate doesn't correctly update them
+			-- Source: AstroNvim
 			"bash",
-			"fish",
-			-- lua
-			"lua",
-			-- typescript
-			"javascript",
-			"jsdoc",
-			"tsx",
-			"typescript",
-			-- python
-			"python",
-			"toml",
-			-- golang
-			"go",
-			-- toml
-			"toml",
-			-- rust
-			"rust",
-			-- c
 			"c",
-			"cpp",
-			-- kdl
-			"kdl",
+			"lua",
+			"markdown",
+			"markdown_inline",
+			"python",
+			"query",
+			"vim",
+			"vimdoc",
+			"fish", -- shell
+			"javascript", -- typescript
+			"jsdoc", -- typescript
+			"tsx", -- typescript
+			"typescript", -- typescript
+			"toml", -- python
+			"go", -- golang
+			"rust", -- rust
+			"cpp", -- c/c++
+			"kdl", -- kdl
 		},
 		highlight = {
 			enable = true,
