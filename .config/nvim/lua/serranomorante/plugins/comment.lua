8a1dfe9055660c14b3c40fd57b46ef7ae53d2be4
Patricio Serrano <serranomorantepatricio@gmail.com>
2023-11-30 23:42:00 +0100

    Improve ts commentstring module loading in preparation for treesitter 1.0

--- .config/nvim/lua/serranomorante/plugins/comment.lua
+++ .config/nvim/lua/serranomorante/plugins/comment.lua
@@ -24,6 +24,7 @@ return {
     },
   },
   opts = function()
+    -- https://github.com/JoosepAlviste/nvim-ts-context-commentstring/wiki/Integrations#commentnvim
     local commentstring_avail, commentstring = pcall(require, "ts_context_commentstring.integrations.comment_nvim")
     return commentstring_avail and commentstring and { pre_hook = commentstring.create_pre_hook() } or {}
   end,
