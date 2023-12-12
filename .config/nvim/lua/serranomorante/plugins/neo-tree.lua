bcfc0bdbfaadb98ece60fe6c6dd54ef4051f047c
Patricio Serrano <serranomorantepatricio@gmail.com>
2023-12-12 17:51:05 +0100

    [indent-o-matic] replace guess-indent.nvim with indent-o-matic.nvim

--- .config/nvim/lua/serranomorante/plugins/neo-tree.lua
+++ .config/nvim/lua/serranomorante/plugins/neo-tree.lua
@@ -82,7 +82,7 @@ return {
         if node.type == "directory" or node:has_children() then
           if not node:is_expanded() then -- if unexpanded, expand
             state.commands.toggle_node(state)
-          else -- if expanded and has children, seleect the next child
+          else -- if expanded and has children, select the next child
             require("neo-tree.ui.renderer").focus_node(state, node:get_child_ids()[1])
           end
         else -- if not a directory just open it
