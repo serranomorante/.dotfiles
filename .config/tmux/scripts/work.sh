f7269b89207d73ac5d91b0a9bc751b809c4a98d9
Patricio Serrano <serranomorantepatricio@gmail.com>
2023-12-18 18:18:17 +0100

    [tmux] minor script improvement

--- .config/tmux/scripts/work.sh
+++ .config/tmux/scripts/work.sh
@@ -7,15 +7,18 @@ session_name="work"
 foam="foam"
 nvim="nvim"
 gems="gems"
-wait_for_shell=0.3
+wait_shell_init=0.3
+wait_shell_cmd=0.001
 
 tmux new-session -d -n $foam -s $session_name # create session, name window and detach
 
 # setup "foam" window
 # ---------------------
-sleep $wait_for_shell # wait for shell to load
+sleep $wait_shell_init # wait for shell to load
 
 tmux send-keys -t "$session_name:$foam" "z foam" Enter # cd into directory
+
+sleep $wait_shell_cmd
 
 tmux send-keys -t "$session_name:$foam" "vim" Enter
 
@@ -23,9 +26,11 @@ tmux send-keys -t "$session_name:$foam" "vim" Enter
 # ---------------------
 tmux new-window -t $session_name -n $nvim # this will automatically focus the new window
 
-sleep $wait_for_shell
+sleep $wait_shell_init
 
 tmux send-keys -t "$session_name:$nvim" "z nvim" Enter # cd into directory
+
+sleep $wait_shell_cmd
 
 tmux send-keys -t "$session_name:$nvim" "vim" Enter
 
@@ -33,16 +38,21 @@ tmux send-keys -t "$session_name:$nvim" "vim" Enter
 # ---------------------
 tmux new-window -t $session_name -n "$gems"
 
-sleep $wait_for_shell
+sleep $wait_shell_init
 
 tmux send-keys -t "$session_name:$gems" "z gems" Enter
+
+sleep $wait_shell_cmd
+
 tmux send-keys -t "$session_name:$gems" "vim" Enter
 
-tmux split-window -t $session_name -h -p 30
+tmux split-window -t $session_name -h -l 20%
 
-sleep $wait_for_shell
+sleep $wait_shell_init
 
 tmux send-keys -t $session_name "z gems" Enter
+
+sleep $wait_shell_cmd
 
 # tmux select-window -t "$session_name:$nvim" # focused window
 
