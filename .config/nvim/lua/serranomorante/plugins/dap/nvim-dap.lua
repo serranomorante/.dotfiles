cc389a4c2d26a42c4aa01fab9a99d6298b6a4bd1
Patricio Serrano <serranomorantepatricio@gmail.com>
2024-01-07 22:41:32 +0100

    [nvim] don't close dapui windows on process termination

--- .config/nvim/lua/serranomorante/plugins/dap/nvim-dap.lua
+++ .config/nvim/lua/serranomorante/plugins/dap/nvim-dap.lua
@@ -79,8 +79,6 @@ return {
 
     dap.listeners.before.attach["dapui_config"] = dapui.open
     dap.listeners.before.launch["dapui_config"] = dapui.open
-    dap.listeners.before.event_terminated["dapui_config"] = dapui.close
-    dap.listeners.before.event_exited["dapui_config"] = dapui.close
 
     ---╔══════════════════════════════════════╗
     ---║               Adapters               ║
