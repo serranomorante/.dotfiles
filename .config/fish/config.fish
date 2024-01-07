2bb56e8879827000473f2a67d83e9f5904fef573
Patricio Serrano <serranomorantepatricio@gmail.com>
2024-01-07 13:35:27 +0100

    [fish] use only block cursor on all vi modes

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -9,18 +9,8 @@ if status is-interactive
     # Emulates vim's cursor shape behavior
     # Set the normal and visual mode cursors to a block
     set fish_cursor_default block blink
-    # Set the insert mode cursor to a line
-    set fish_cursor_insert line blink
-    # Set the replace mode cursor to an underscore
-    set fish_cursor_replace_one underscore blink
-    # The following variable can be used to configure cursor shape in
-    # visual mode, but due to fish_cursor_default, is redundant here
-    set fish_cursor_visual block blink
     # https://github.com/fish-shell/fish-shell/issues/5894
     set -g fish_escape_delay_ms 10
-
-    # Force cursor switch
-    set fish_vi_force_cursor true
 
     # Use fzf to search history
     function reverse_history_search
