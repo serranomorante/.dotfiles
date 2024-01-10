0481a825ac065ad039ffccbe72e1aa5d0fd7ecc6
Patricio Serrano <serranomorantepatricio@gmail.com>
2024-01-10 19:04:46 +0100

    [nvim] improve nightfox highlights on diff view

--- .config/nvim/lua/serranomorante/plugins/nightfox.lua
+++ .config/nvim/lua/serranomorante/plugins/nightfox.lua
@@ -2,22 +2,43 @@ return {
   "EdenEast/nightfox.nvim",
   lazy = false,
   priority = 1000, -- make sure to load this before all the other start plugins
-  opts = {
-    options = {
-      transparent = true,
-      dim_inactive = false,
-    },
-    groups = {
-      nightfox = {
-        -- Make window splits more obvious
-        WinSeparator = { fg = "bg2" },
-        -- Dim the folded highlight
-        Folded = { bg = "bg0" },
-        -- Differentiate CodeLens from comments
-        LspCodeLens = { bg = "bg0" },
+  opts = function()
+    local opts = {
+      options = {
+        transparent = true,
+        dim_inactive = false,
       },
-    },
-  },
+      groups = {
+        nightfox = {
+          ---Make window splits more obvious
+          WinSeparator = { fg = "bg2" },
+          ---Dim the folded highlight
+          Folded = { bg = "bg0" },
+          ---Differentiate CodeLens from comments
+          LspCodeLens = { bg = "bg0" },
+        },
+      },
+    }
+
+    local C = require("nightfox.lib.color")
+    local palette = require("nightfox.palette").load("nightfox")
+    ---Improve nightfox colors on diff view
+    local DiffViewAdd = C(palette.bg0):blend(C(palette.green.dim), 0.25):to_css()
+    local DiffViewChange = C(palette.bg0):blend(C(palette.blue.dim), 0.10):to_css()
+    local DiffViewText = C(palette.bg0):blend(C(palette.cyan.dim), 0.5):to_css()
+
+    opts.specs = {
+      nightfox = {
+        diff = {
+          add = DiffViewAdd,
+          change = DiffViewChange,
+          text = DiffViewText,
+        },
+      },
+    }
+
+    return opts
+  end,
   config = function(_, opts)
     require("nightfox").setup(opts)
 
