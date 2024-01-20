ddbf1db2aa04b188c6ecc60e252f71d2e634a718
Patricio Serrano <serranomorantepatricio@gmail.com>
2024-01-20 11:52:53 +0100

    [nvim] add dressing to improve select UI
    When using nvim-dap process picker, if there are a lot of processes the
    native neovim select makes it really difficult to pick a choice.

---
+++ .config/nvim/lua/serranomorante/plugins/dressing.lua
@@ -0,0 +1,10 @@
+return {
+  "stevearc/dressing.nvim",
+  opts = {
+    input = {
+      default_prompt = "➤ ",
+      insert_only = false,
+    },
+    select = { backend = { "telescope", "builtin" } },
+  },
+}
