469091822223b264cea943fd4f83db8ed9fff41d
Patricio Serrano <serranomorantepatricio@gmail.com>
2024-01-21 23:39:00 +0100

    [nvim] re-add resession.nvim VimLeavePre after fix

--- .config/nvim/lua/serranomorante/plugins/resession.lua
+++ .config/nvim/lua/serranomorante/plugins/resession.lua
@@ -83,5 +83,14 @@ return {
         callback = autoload_session,
       })
     end
+
+    vim.api.nvim_create_autocmd("VimLeavePre", {
+      desc = "Save a dir-specific session when you close Neovim",
+      group = vim.api.nvim_create_augroup("resession_autosave_session", { clear = true }),
+      callback = function()
+        -- Only save the session if nvim was started with no args
+        if vim.fn.argc(-1) == 0 then resession.save_tab(vim.fn.getcwd(), { dir = "dirsession", notify = false }) end
+      end,
+    })
   end,
 }
