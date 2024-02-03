3772b9ff6b5d05be67f11d989ceaf82bf86b6810
Patricio Serrano <serranomorantepatricio@gmail.com>
2024-02-03 12:31:25 +0100

    [fish] just use git prompt directly on shell
    This might be an unnecessary change but it doesn't matter.
    
    [See](https://fishshell.com/docs/current/cmds/fish_git_prompt.html)

--- .config/fish/functions/fish_prompt_original.fish
+++ .config/fish/functions/fish_prompt_original.fish
@@ -27,5 +27,5 @@ function fish_prompt_original --description 'Write out the prompt'
     set -l statusb_color (set_color $bold_flag $fish_color_status)
     set -l prompt_status (__fish_print_pipestatus "[" "]" "|" "$status_color" "$statusb_color" $last_pipestatus)
 
-    echo -n -s (prompt_login)' ' (set_color $color_cwd) (prompt_pwd) $normal (fish_vcs_prompt) $normal " "$prompt_status $suffix " "
+    echo -n -s (prompt_login)' ' (set_color $color_cwd) (prompt_pwd) $normal (fish_git_prompt) $normal " "$prompt_status $suffix " "
 end
