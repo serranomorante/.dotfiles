fbc157ba434da40c51feaba00ff702427bb0fbb9
Patricio Serrano <serranomorantepatricio@gmail.com>
2024-02-06 23:53:05 +0100

    [nvim] add one-small-step-for-vimkind

---
+++ .config/nvim/lua/serranomorante/plugins/dap/one-small-step-for-vimkind.lua
@@ -0,0 +1,26 @@
+return {
+  "jbyuki/one-small-step-for-vimkind",
+  dependencies = "mfussenegger/nvim-dap",
+  event = "User CustomLoadLuaDAP",
+  keys = {
+    {
+      "<leader>cc",
+      [[:lua require"osv".launch({port = 8086})<CR>]],
+      desc = "Launch one small step for vimkind",
+    },
+  },
+  config = function()
+    local dap = require("dap")
+    dap.configurations.lua = {
+      {
+        type = "nlua",
+        request = "attach",
+        name = "Attach to running Neovim instance",
+      },
+    }
+
+    dap.adapters.nlua = function(callback, config)
+      callback({ type = "server", host = config.host or "127.0.0.1", port = config.port or 8086 })
+    end
+  end,
+}
