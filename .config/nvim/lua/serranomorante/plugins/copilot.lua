79554840f0b1cc7f0912c282755fb01586d2e7b5
Patricio Serrano <serranomorantepatricio@gmail.com>
2024-03-02 16:19:13 +0100

    [nvim] add copilot.vim

---
+++ .config/nvim/lua/serranomorante/plugins/copilot.lua
@@ -0,0 +1,19 @@
+return {
+  "github/copilot.vim",
+  ---Copilot's startup time is slow. I cannot use regular 'InsertEnter' event as Telescope also triggers
+  ---that event when switching buffers in insert mode causing a noticeable delay.
+  event = "User CustomInsertEnter",
+  init = function()
+    vim.g.copilot_node_command = vim.g.node_system_executable
+    vim.g.copilot_no_tab_map = true -- I use `tab` for completion
+  end,
+  config = function()
+    vim.api.nvim_exec_autocmds("InsertEnter", { buffer = vim.api.nvim_get_current_buf(), modeline = false })
+
+    vim.keymap.set("i", "<M-c>", 'copilot#Accept("\\<CR>")', {
+      expr = true,
+      replace_keycodes = false,
+      desc = "Copilot: Accept suggestion",
+    })
+  end,
+}
